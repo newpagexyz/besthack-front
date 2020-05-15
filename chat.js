0e218e10db91789aa118df6d1905667d40dec98c
denis <d_volkov2@bk.ru>
2020-05-16 01:51:10 +0300

    гребаная прокрутка ууууу огрессия

---
+++ chat.js
@@ -0,0 +1,96 @@
+const mainContainer = document.getElementById('message-slider');
+
+
+class Message {
+    constructor(owner, text, avatarUrl) {
+        this.owner = owner;
+        this.text = text;
+        this.avatar = avatarUrl;
+    }
+}
+
+class RecyclerView {
+    constructor(container, maxMessages) {
+        this.container = container;
+        this.maxMessages = maxMessages;
+        this.messages = [];
+        this.pos = 0;
+
+        this.divs = [];
+        this.avatars = [];
+        this.texts = [];
+
+        for (let i = 0; i < maxMessages; i++) {
+            const div = document.createElement('div');
+            div.className = 'columns';
+
+            const firstColumn = document.createElement('div');
+            firstColumn.className = 'column is-one-fifth';
+            const avatar = document.createElement('img');
+            avatar.className = 'image is-128x128 is-rounded';
+            firstColumn.append(avatar);
+
+            const secondColumn = document.createElement('div');
+            secondColumn.className = 'column';
+            const text = document.createElement('p');
+            secondColumn.append(text);
+
+            this.divs.push(div);
+            this.avatars.push(avatar);
+            this.texts.push(text);
+
+            div.append(firstColumn);
+            div.append(secondColumn);
+
+            this.container.append(div);
+        }
+    }
+
+    render() {
+        for (let i = 0; i < Math.min(this.maxMessages, this.messages.length); i++) {
+            this.avatars[this.maxMessages - 1 - i].src = this.messages[this.pos + i].avatar;
+            this.texts[this.maxMessages - 1 - i].innerText = this.messages[this.pos + i].text;
+        }
+    }
+
+
+    append(message) {
+        this.messages.push(message);
+    }
+
+    init() {
+        this.container.addEventListener('scroll', () => {
+            const currentScroll = this.container.scrollTop;
+            if (currentScroll < 10) {
+                this.pos += this.maxMessages;
+                this.divs[this.divs.length - 1].scrollIntoView(true);
+                this.render();
+                return;
+            }
+            if (currentScroll > this.container.clientHeight) {
+                console.log("wut");
+                return;
+                this.pos -= this.maxMessages;
+                this.divs[0].scrollIntoView(false);
+                this.render();
+                return;
+            }
+
+
+        });
+    }
+
+}
+
+function testMessages(recycler) {
+    for (let i = 0; i < 100; i++) {
+        const msg = new Message('kek', 'это номер' + i,
+            'avatar.jpg');
+        recycler.append(msg);
+    }
+    recycler.render();
+}
+
+const myRecyclerView = new RecyclerView(mainContainer, 10);
+myRecyclerView.init();
+testMessages(myRecyclerView);
